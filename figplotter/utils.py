2d5fd99721aa78a9f1a761879d2839c51464ec38
Javier Cabezas <javier.cabezas@bsc.es>
2014-12-18 15:49:12 +0100

    Add clusterize function to create cluster trees from arrays of values

--- figplotter/utils.py
+++ figplotter/utils.py
@@ -1,3 +1,5 @@
+from collections import OrderedDict
+
 def range_generator(start, stop):
     assert start < stop, "Wrong values for range generator"
     distance = float(stop - start)
@@ -18,3 +20,47 @@ def param(l):
 def string_generator(pattern, values):
     return lambda x: (pattern % value for value in values(x))
 
+def clusterize(series, clusters):
+    len_series = -1
+    for k, v in series.items():
+        if len_series == -1:
+            len_series = len(v)
+        else:
+            assert len_series == len(v), 'All series must have the same number of values'
+
+    nclusters = 1
+    for cluster_level in clusters:
+        nclusters = nclusters * len(cluster_level)
+
+    assert nclusters == len_series, 'Series must contain as many values as clusters'
+
+    def leafs(d):
+        if len(d.items()) == 0:
+            return [ d ]
+        else:
+            ret = []
+            for k, v in d.items():
+                ret += leafs(v)
+
+        return ret
+
+    def walk(d, fun):
+        if len(d.items()) == 0:
+            fun(d)
+        else:
+            for k, v in d.items():
+                walk(v, clusters)
+
+    ret = OrderedDict()
+    for cluster_level in clusters:
+        def insert_cluster(d):
+            for cluster in cluster_level:
+                d[cluster] = OrderedDict()
+
+        walk(ret, insert_cluster)
+
+    for i, leaf in enumerate(leafs(ret)):
+        for k, series_vals in series.items():
+            leaf[k] = series_vals[i]
+
+    return ret
--- tests/cluster.py
+++ tests/cluster.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 import figplotter
 
-from figplotter.utils import Parameter as P
+from figplotter.utils import Parameter as P, clusterize
 from figplotter.plot import figure, cluster_series
 
 def figure_simple_cluster():
@@ -31,6 +31,28 @@ def figure_simple_cluster():
                    })
     fig.show()
 
+def figure_simple_clusterize():
+    fig = figure()
+    ax = fig.add_subplot(111)
+    read  = [1, 2,   4,    5]
+    write = [2, 3, 3.5, 3.75]
+
+    clusters = [ 'local', 'remote', 'copy', 'peak' ]
+    series = clusterize({'Read': read, 'Write': write}, [clusters])
+
+    cluster_series(ax,
+                   series,
+                   clusters,
+                   bar_params = {
+                       'color' : P({
+                           'Read': 'b',
+                           'Write': 'g',
+                       })
+                   })
+    fig.show()
+
+
 figure_simple_cluster()
+figure_simple_clusterize()
 
 plt.show()
